ff0e5861b25dcfb96b552c4b1b218ac72c7e8e91
SuprimDevkota <suprimdevkota@gmail.com>
2021-06-13 09:10:41 +0545

    Modified comments

--- Recursive 1's.c
+++ Recursive 1's.c
@@ -35,15 +35,22 @@ int sumterms_without_recursion(int n)
   return sum;
 }
 
-/*So, we come to the heart of the problem. Recursion basically means to call a function from within itself. As programmers, recursion will play a big role in what we do later on so I'll try to give you a step by step approach that worked for me.
+/*So, we come to the heart of the problem. Recursion basically means to call a 
+function from within itself. As programmers, recursion will play a big role in 
+what we do later on so I'll try to give you a step by step approach that worked 
+for me.
 
 For a recursive problem there are basically three steps:
 
-The first step is to think of a "base case". This is best visualized by thinking of the simplest input to the function. In our case, it will be sum(1) = 1.
+The first step is to think of a "base case". This is best visualized by thinking
+ of the simplest input to the function. In our case, it will be sum(1) = 1.
 
-The second step is to think how the base case can be used to solve the larger problem. It will be helpful to focus on a single like 1 or 2 and try to come up with a recursive function that gives us the desired output.
+The second step is to think how the base case can be used to solve the larger 
+problem. It will be helpful to focus on a single like 1 or 2 and try to come 
+up with a recursive function that gives us the desired output.
 
-The third step is to generalize this function for other terms so that we can solve the problem.
+The third step is to generalize this function for other terms so that we
+ can solve the problem.
  */
 
 int sumterms_with_recursion(int n)
@@ -58,22 +65,26 @@ int sumterms_with_recursion(int n)
   }
 }
 
-/*In the above function we set our base case to be when n = 1. When our base case occurs we want to return 10^0 = 1. This is also the condition that will prevent the recursive call from running indefintely. 
-What if we don't encounter our base case? Now this is the tricky part. This is where you have to use the recursive function that you thought of. Note that the term n will decrease with every recursive call.
+/*In the above function we set our base case to be when n = 1. When 
+our base case occurs we want to return 10^0 = 1. This is also the 
+condition that will prevent the recursive call from running indefintely. 
+What if we don't encounter our base case? Now this is the tricky part.
+This is where you have to use the recursive function that you thought of. 
+Note that the term n will decrease with every recursive call.
 
 In our case if we consider N = 3: 
 
-  The base case will be sumterms_with_recursion(1) = 1
-  If we take N = 2 then sumterms_with_recursion(2) = (10^0) + (10^0 + 10^1)
-  Taking N = 3, sumterms_with_recursion(3) = (10^0) + (10^0 + 10^1) + (10^0 + 10^1 + 10^2)
-  that is:
-  sumterms_with_recursion(3) = sumterms_with_recursion(2) + (1 - pow(10,3))/(1 - 10)
+The base case will be sumterms_with_recursion(1) = 1
+If we take N = 2 then sumterms_with_recursion(2) = (10^0) + (10^0 + 10^1)
+Taking N = 3, sumterms_with_recursion(3) = (10^0) + (10^0 + 10^1) + (10^0 + 10^1 + 10^2)
+that is:
+sumterms_with_recursion(3) = sumterms_with_recursion(2) + (1 - pow(10,3))/(1 - 10)
 
-  The second term in the function is a formula for geometric series where a = 1, r = 10 and n = 3
+The second term in the function is a formula for geometric series where a = 1, r = 10 and n = 3
 
 
-  Generalising this:
-  sumterms_with_recursion(n) = sumterms_with_recursion(n-1) + (1 - pow(10,n))/(1 - 10);
+Generalising this:
+sumterms_with_recursion(n) = sumterms_with_recursion(n-1) + (1 - pow(10,n))/(1 - 10);
 */
 
 
